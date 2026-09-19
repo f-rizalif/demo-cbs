7a8fae7dec0bf19c8c5d2a3992202c08b036ad80
F Rizal <alif.rizalif@gmail.com>
2026-09-20 03:20:18 +0700

    deploy demo 2026-09-20_03-20-18

--- assets/demoFinishGood-T9xupu2w.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/finish-good-main-BEurzafo.js","assets/jsx-runtime-JC7fXDIz.js","assets/client-BLQq0x_E.js","assets/react-dom-nPHVLlTK.js","assets/HomeShell-CQ-scoj8.js","assets/doodle-C-_yXIcD.js","assets/navigasi-Bz9yd0M4.js","assets/src-BAkZzJf5.css"])))=>i.map(i=>d[i]);
-import"./keadaan-C8ZNwV-b.js";import{n as e,t}from"./preload-helper-C8wJuyn0.js";e(),await t(()=>import(`./finish-good-main-BEurzafo.js`),__vite__mapDeps([0,1,2,3,4,5,6,7]));
